84dd82050b2abe11121bdae301d7cc4a5178d7ad
raulzrosa <raulzrosa@gmail.com>
2015-11-09 04:50:36 -0200

    Versão final da versão do algoritmo smooth com execução paralela utilizando OpenMP

--- SmoothMP.cpp
+++ SmoothMP.cpp
@@ -1,5 +1,8 @@
-/*	g++ -o smooth smooth.cpp `pkg-config --cflags --libs opencv`
-	g++ -o paralelo paralelo.cpp `pkg-config --cflags --libs opencv` -fopenmp
+/*	
+	g++ -o SmoothMP SmoothMP.cpp `pkg-config --cflags --libs opencv` -fopenmp
+	./SmoothMP image_in type_img image_out
+	type_img -> 0 = GRAYSCALE
+	type_img -> 1 = COLOR
 */
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -16,14 +19,14 @@ using namespace std;
 
 //matriz com a imagem de entrada
 Mat in;
-Mat *out_p;
+Mat out_p;
 //border = 1 pois tamanho da máscara = 5 => floor(5/2) = 2
 int border = 2;
 
 void aplica_smooth_grayscale(int ini_i, int ini_j, int end_i, int end_j) {
 	float average;
-	for(int i = ini_i; i < end_i; i++) {
-		for(int j = ini_j; j < end_j; j++) {
+	for(int i = ini_i + border; i < end_i - border; i++) {
+		for(int j = ini_j + border; j < end_j - border; j++) {
 			average = in.at<uchar>(i, j) 
 			+ in.at<uchar>(i + 1, j) 
 			+ in.at<uchar>(i - 1, j) 
@@ -50,15 +53,14 @@ void aplica_smooth_grayscale(int ini_i, int ini_j, int end_i, int end_j) {
 			+ in.at<uchar>(i - 2, j + 2)
 			+ in.at<uchar>(i - 2, j - 2);
 			average = average/25;	
-			out_p->at<uchar>(i - 2, j - 2) = (uchar)average;
+			out_p.at<uchar>(i - border, j - border) = (uchar)average;
 		}
 	}
-	//printf("OLAR!\n");
 }
 void aplica_smooth_color(int ini_i, int ini_j, int end_i, int end_j) {
 	float average;
-	for(int i = ini_i; i < end_i; i++) {
-		for(int j = ini_j; j < end_j; j++) {
+	for(int i = ini_i + border; i < end_i - border; i++) {
+		for(int j = ini_j + border; j < end_j - border; j++) {
 			for(int color = 0; color < 3; color++) {
 				average = in.at<cv::Vec3b>(i, j)[color] 
 				+ in.at<cv::Vec3b>(i + 1, j)[color] 
@@ -87,19 +89,20 @@ void aplica_smooth_color(int ini_i, int ini_j, int end_i, int end_j) {
 				+ in.at<cv::Vec3b>(i + 2, j - 2)[color];
 
 				average = average/25;
-				out_p->at<cv::Vec3b>(i, j)[color] = (uchar)average;
+				out_p.at<cv::Vec3b>(i - border, j - border)[color] = (uchar)average;
 			}
 		}
 	}
-	//printf("OLAR!\n");
 }
-void smooth(int tipo_img) {
+int smooth(int tipo_img) {
 
 	//copia imagem de entrada
 	if(tipo_img == 0) {
-		out_p = new Mat(in.size(), CV_8U, 1);
+		out_p = Mat(in.size(), CV_8U, 1);
+	} else if(tipo_img == 1) {
+		out_p = Mat(in.size(), CV_8UC3, 1);
 	} else {
-		out_p = new Mat(in.size(), CV_8UC3, 1);
+		return 0;
 	}
 	
 	//achar a média aritmética e depois atualizar o pixel
@@ -108,25 +111,24 @@ void smooth(int tipo_img) {
 	//int ini_i = 0, ini_j = 0, end_i = 100, end_j = 100;
 	int ini_i[4],ini_j[4],end_i[4],end_j[4];
 	int meio_w, meio_h;
+	//replica a borda pra solucionar o problema dos pixels de borda
+	copyMakeBorder(in, in, border, border, border, border, BORDER_REPLICATE);
 	//calcula o meio da imagem
 	meio_h = in.size().height/2;
 	meio_w = in.size().width/2;
 	//calcula o inicio de cada pedaço da imagem
-	ini_i[0] = 0 + border; 		ini_j[0] = 0 + border;
-	ini_i[1] = 0 + border; 		ini_j[1] = meio_w;
-	ini_i[2] = meio_h; 			ini_j[2] = 0 + border;
-	ini_i[3] = meio_h; 			ini_j[3] = meio_w;
+	ini_i[0] = 0; 							ini_j[0] = 0;
+	ini_i[1] = 0; 							ini_j[1] = meio_w - border;
+	ini_i[2] = meio_h - border; 			ini_j[2] = 0;
+	ini_i[3] = meio_h - border; 			ini_j[3] = meio_w - border;
 	//calcula o fim de cada pedaço da imagem
-	end_i[0] = meio_h; 						end_j[0] = meio_w;
-	end_i[1] = meio_h; 						end_j[1] = in.size().width - border;
-	end_i[2] = in.size().height - border; 	end_j[2] = meio_w;
-	end_i[3] = in.size().height - border;	end_j[3] = in.size().width - border;
-
-	//replica a borda pra solucionar o problema dos pixels de borda
-	copyMakeBorder(in, in, border, border, border, border, BORDER_REPLICATE);
+	end_i[0] = meio_h + border; 			end_j[0] = meio_w + border;
+	end_i[1] = meio_h + border; 			end_j[1] = in.size().width;
+	end_i[2] = in.size().height;		 	end_j[2] = meio_w + border;
+	end_i[3] = in.size().height;			end_j[3] = in.size().width;
 
 	//chama cada iteração do for junto em paralelo passando como parametro o 
-	//inicio e o fim de cada pedaço da imagem que cada thread deve rodar
+	//inicio e o fim de cada pedaço da imagem qu	e cada thread deve rodar
 	#pragma omp parallel for shared(in,out_p, end_i, end_j, ini_i, ini_j)
 		for(int i = 0; i < NTHREADS; i ++) {
 			if(tipo_img == 0) {
@@ -135,67 +137,9 @@ void smooth(int tipo_img) {
 				aplica_smooth_color(ini_i[i], ini_j[i], end_i[i], end_j[i]);
 			}
 		}
+	return 1;
 }
 
-//função que aplica o filtro em imagem colorida
-//sera necessário utilizar função MPI para paralelizar cada cor (R, G e B) em diferentes nós
-//esse tipo de composição é uma decomposição funcional pois cada cor da imagem é idenpendente, ou seja,
-//nao precisam compartilhar o mesmo dado.
-//ja para cada cor, vamor fazer uma decomposição por domínio: vamos dividir a imagem pelo número de nucleos
-//que o processador possui. Para isso vamos utilizar o OpenMP
-/*Mat *aplica_smooth_color() {
-
-	//copia imagem de entrada 
-	Mat *out = new Mat(in->size(), CV_8UC3, 1);
-	Mat aux(in->size(), CV_8UC3, 1);
-
-	//border = 1 pois tamanho da máscara = 5 => floor(5/2) = 2
-	int border = 2;
-	
-	//replica a borda pra solucionar o problema dos pixels de borda
-	copyMakeBorder(*in, aux, border, border, border, border, BORDER_REPLICATE);
-
-	//achar a média aritmética e depois atualizar o pixel
-	float average;
-
-	for(int i = border; i < in->size().height - border; i++) {
-		for(int j = border; j < in->size().width - border; j++) {
-			//nao sei se tenho que somar como uchar ou float]
-			for(int color = 0; color < 3; color++) {
-				average = in->at<cv::Vec3b>(i, j)[color] 
-				+ in->at<cv::Vec3b>(i + 1, j)[color] 
-				+ in->at<cv::Vec3b>(i - 1, j)[color]
-				+ in->at<cv::Vec3b>(i, j + 1)[color] 
-				+ in->at<cv::Vec3b>(i, j - 1)[color]
-				+ in->at<cv::Vec3b>(i - 2, j)[color] 
-				+ in->at<cv::Vec3b>(i + 2, j)[color] 
-				+ in->at<cv::Vec3b>(i, j + 2)[color]
-				+ in->at<cv::Vec3b>(i, j - 2)[color]
-				+ in->at<cv::Vec3b>(i + 1, j + 1)[color]
-				+ in->at<cv::Vec3b>(i + 1, j + 2)[color] 
-				+ in->at<cv::Vec3b>(i + 1, j - 1)[color] 
-				+ in->at<cv::Vec3b>(i + 1, j - 2)[color]
-				+ in->at<cv::Vec3b>(i - 1, j - 1)[color] 
-				+ in->at<cv::Vec3b>(i - 1, j + 1)[color]
-				+ in->at<cv::Vec3b>(i - 1, j + 2)[color]
-				+ in->at<cv::Vec3b>(i - 1, j - 2)[color] 
-				+ in->at<cv::Vec3b>(i - 2, j - 2)[color] 
-				+ in->at<cv::Vec3b>(i - 2, j + 1)[color]
-				+ in->at<cv::Vec3b>(i - 2, j - 1)[color]
-				+ in->at<cv::Vec3b>(i - 2, j + 2)[color]
-				+ in->at<cv::Vec3b>(i + 2, j + 2)[color]
-				+ in->at<cv::Vec3b>(i + 2, j + 1)[color] 
-				+ in->at<cv::Vec3b>(i + 2, j - 1)[color]
-				+ in->at<cv::Vec3b>(i + 2, j - 2)[color];
-
-				average = average/25;
-				out->at<cv::Vec3b>(i, j)[color] = (uchar)average;
-			}
-
-		}
-	}
-	return out;
-}*/
 
 int main(int argc, char *argv[]) {
 	//diz se a imagem é grayscale or color
@@ -223,13 +167,14 @@ int main(int argc, char *argv[]) {
 		cout << "Nao foi possivel abrir a  imagem: " << endl;
 		return -1;
 	}
-	printf("ENTROU\n");
+
 	//aplica algoritmo smooth e recebe a nova imagem
-	smooth(tipo_img);
-
-	imwrite(fileOut, *out_p);
-
+	if(smooth(tipo_img) == 1) {
+		imwrite(fileOut, out_p);
+	} else {
+		printf("Nao foi possivel criar nova imagem.\n");
+	}
 	in.release();
-	out_p->release();
+	out_p.release();
     return 0;
 }
